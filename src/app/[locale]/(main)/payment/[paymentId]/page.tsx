4fe5c1cf055226c3e9a53ce2e25b7eabac0604be
Fink2005 <phans3806@gmail.com>
2025-07-29 06:35:50 +0700

    update

---
+++ src/app/[locale]/(main)/payment/[paymentId]/page.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
+const page = () => {
+  return (
+    <div className="min-h-screen">
+      <img src="https://qr.sepay.vn/img?acc=0847979884&bank=VPbank&amount=2000&des=PTS1" alt="" />
+
+    </div>
+  );
+};
+
+export default page;
