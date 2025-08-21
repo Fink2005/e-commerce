94c23f6eedbbe9b69f0d7bc84325857400125e90
Tai874vn <tai874vn@gmail.com>
2025-08-21 15:51:12 +0700

    fix home page Font

--- src/components/home/SectionBadge.tsx
+++ src/components/home/SectionBadge.tsx
@@ -6,7 +6,7 @@ const SectionBadge = ({ name }: SectionBadgeProps) => {
   return (
     <div className="flex items-center gap-2">
       <div className="w-3 h-6 bg-red-500 rounded-full">&nbsp;</div>
-      <p className="text-red-500 text-sm font-semibold">{name}</p>
+      <p className="text-red-500 text-2xl font-bold">{name}</p>
     </div>
   );
 };
